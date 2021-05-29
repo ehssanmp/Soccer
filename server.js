d15e01820267fb9fc2fea9ca7b8f7e556e132ca1
mousavipou <sy778393@dal.ca>
2021-05-29 13:02:55 -0300

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 const http= require('http');
-const { path } = require('./app');
+const express = require('express');
+const app = express();
+const cors=require('cors');
+const request=require('request');
+const path=require('path')
+const port =process.env.PORT||5000;
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'build')))
+  app.use(cors())
+  app.post('/email',(req,res)=>{
+    const data={
+        members:[
+            {
+                email_address:req.body.email,
+                status:'subscribed'
+            }
+        ]
+    }
+    const postData=JSON.stringify(data);
+    const options={
+        url:'https://us6.api.mailchimp.com/3.0/lists/5bdbc1f8c6',
+        method:'POST',
+        headers:{
+            Authorization:'auth 8e71c0e8916746d5db141ace553ed385-us6'
+        },
+        body:postData
+    }
+    request(options,(err,response,body)=>{
+        if(response.statusCode===200){
+            console.log("hi")
+        }
+        else{
+            
+            res.status(401).json({
+                error:err
+            })
+        }
+    })
+  })
 
-const port =process.env.PORT||8080;
-const app=require('./app');
-const server=http.createServer(app);
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/build/index.html'))
+    res.sendFile(Path.join(__dirname+'/build/index.html'))
 })
-server.listen(port);
+app.listen(port,console.log("fine"))
